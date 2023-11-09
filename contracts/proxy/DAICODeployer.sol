d0640ae38cd88ed7fee22b15c54b78b08cf5bdfa
boyuanx <boyuanxu@usc.edu>
2023-11-09 17:08:38 +0300

    Optimize imports

--- contracts/proxy/DAICODeployer.sol
+++ contracts/proxy/DAICODeployer.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {ICOVault, ICOVaultExchangeRateDataSource} from "../ICOVault.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import {DAICOCustom} from "../interfaces/DAICOCustom.sol";
 
