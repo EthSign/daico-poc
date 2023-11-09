9ebe423e2a0fa5b73a55e04bb91ec7e0354988ff
boyuanx <boyuanxu@usc.edu>
2023-11-09 17:09:49 +0300

    Updated mock data source

--- contracts/mock/MockICOVaultExchangeRateDataSource.sol
+++ contracts/mock/MockICOVaultExchangeRateDataSource.sol
@@ -5,10 +5,6 @@ import {ICOVaultExchangeRateDataSource} from "../ICOVault.sol";
 
 // solhint-disable no-unused-vars
 contract MockICOVaultExchangeRateDataSource is ICOVaultExchangeRateDataSource {
-    function tokensPerFundingUnit() external pure returns (uint256) {
-        return 1;
-    }
-
     function tokensPerFundingUnit(
         address asset
     ) external pure returns (uint256) {
